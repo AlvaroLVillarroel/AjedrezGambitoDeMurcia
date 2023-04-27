c4a9cf1ee2f2f363e2c45fba45918c7a52400722
t.kosanich@alumnos.upm.es <t.kosanich@alumnos.upm.es>
2023-04-27 13:43:35 +0200

    src bin y lib

--- AjedrezGambitoDeMurcia-master/Vector2d.cpp
+++
@@ -1,7 +0,0 @@
-#include "Vector2d.h"
-
-Vector2d::Vector2d(float x, float y)
-{
-	this->x = x;
-	this->y = y;
-}
--- AjedrezGambitoDeMurcia-master/Ventana.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-class Ventana {
-public:
-	float height=800;
-	float width=600;
-};
