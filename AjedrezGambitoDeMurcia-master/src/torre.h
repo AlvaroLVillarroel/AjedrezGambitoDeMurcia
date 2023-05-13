f8c9e919f26c2058e4b48e70810b9c0fde5acb30
Diego Carasatorre Parra <diegocarasad@outlook.com>
2023-05-13 16:01:39 +0200

    peon.cpp en src

--- AjedrezGambitoDeMurcia-master/src/torre.h
+++ AjedrezGambitoDeMurcia-master/src/torre.h
@@ -8,5 +8,13 @@ public:
 	void dibujarPieza();
 	void moverPieza(float fil, float col);
 	void sumar_movimiento();
+	float getPosFila() {
+		return Coord.getFila();
+	}
+	float getPosColumna() {
+		return Coord.getColumna();
+	}
+	void mousePress(int button, int state, int x, int y);
+	void mouseMotion(float x, float y);
 };
 
